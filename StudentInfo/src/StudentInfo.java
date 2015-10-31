2f9e3eb77929c3a93eb9a689f1cf08543f9da51f
1jungeun1 <jungagee@gmail.com>
2015-11-01 01:13:12 +0900

    수정

--- StudentInfo/src/StudentInfo.java
+++ StudentInfo/src/StudentInfo.java
@@ -37,20 +37,90 @@ public class StudentInfo {
 
 	}
 
+
+public class StudentAdd {
+    
+    
+    public void addStudent() throws IOException{
+        
+        Scanner scan = new Scanner(System.in);
+        String stu;
+        System.out.println("학생 정보 입력");
+        stu = scan.nextLine();
+        int lastLine =0;
+        
+        FileWriter f_writer = new FileWriter("test.txt",true);
+        BufferedWriter b_writer = new BufferedWriter(f_writer,1024);
+        b_writer.write(stu);
+        b_writer.write("\n");
+        b_writer.close();
+        f_writer.close();
+        
+        
+        System.out.println("학생정보가 파일에 저장되었습니다.");
+        
+        System.out.println("파일 읽기 결과");
+        
+        FileReader f_reader2 = new FileReader("test.txt");
+        BufferedReader b_reader2 = new BufferedReader(f_reader2,1024);
+        for(int j =0 ; j < 10 ; j++){
+            stu= b_reader2.readLine();
+            System.out.println(String.valueOf(stu));
+            
+        }
+        
+        
+    }
+    
+}
+
+public class StudentInfo {
+    
+    public static void main(String args[]) throws IOException {
+        
+        int input;
+        Scanner sc = new Scanner(System.in);
+        
+        
+        do {
+            
+            System.out.println("«–ª˝¡§∫∏Ω√Ω∫≈€¿‘¥œ¥Ÿ");
+            System.out.println("π¯»£∏¶ ¿‘∑¬«ÿ ¡÷ººø‰");
+            System.out.println("--------------");
+            System.out.println("1. Add");
+            System.out.println("2. Update");
+            System.out.println("3. Delete");
+            System.out.println("4. View");
+            System.out.println("5. ¡æ∑·");
+            System.out.println("--------------");
+            
+            input = sc.nextInt();
+            
+            if (input != 5) {
+                
+                WhatIsInput ip = new WhatIsInput();
+                ip.goSystem(input);
+            }
+            
+        } while (input != 5);
+        
+    }
+    
 }
 
 class WhatIsInput {
-
-	public void goSystem(int input) {
-
-		switch (input) {
-
-		case 1: {
-			StudentAdd sa = new StudentAdd();
-			break;
-		}
-		case 2: {
-			StudentDelete sd = new StudentDelete();
+    
+    public void goSystem(int input) {
+        
+        switch (input) {
+                
+            case 1: {
+                StudentAdd sa = new StudentAdd();
+                sa.addStudent();
+                break;
+            }
+            case 2: {
+                StudentDelete sd = new StudentDelete();
 			break;
 
 		}
@@ -73,7 +143,7 @@ class WhatIsInput {
 	}
 }
 
-class Position{
+class Position {
 
 	public int position(int id){
 
